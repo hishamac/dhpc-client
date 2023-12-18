e9082605816a0cdfaf4e476acc44003cd949ed2a
MUHAMMED HISHAM <cdg.prjs@gmail.com>
2023-12-18 17:56:46 +0530

    Update styles and components

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,7 @@
+import Login from "@/components/Login";
+
 export default function page() {
   return (
-    <div>
-      <h1>Page</h1>
-    </div>
+    <Login />
   );
 }
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,3 +1,14 @@
+"use client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
 export default function Login() {
-    
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const router = useRouter();
+
+  const HandleLogin = async (data: any) => {};
+
+  return <div className="flex"></div>;
 }
